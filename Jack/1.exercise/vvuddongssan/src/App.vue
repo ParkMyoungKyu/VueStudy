b910fda9742b35c18242cecfeb6d1b9d4d5032df
AngLee <nine6ny@nate.com>
2021-10-16 07:03:49 +0900

    이벤트 핸들러 숙제

--- Jack/1.exercise/vvuddongssan/src/App.vue
+++ Jack/1.exercise/vvuddongssan/src/App.vue
@@ -1,8 +1,15 @@
 <template>
+  <div class="menu">
+    <a>Home</a>
+    <a>Product</a>
+    <a>About</a>
+  </div>
   <img alt="Vue logo" src="./assets/logo.png">
-  <div v-for="item in rooms" :key="item">
+  <div v-for="(item,i) in rooms" :key="i">
     <h4 :style="roomStyle">{{ item.roomName }} 원룸</h4>
     <p :style="priceStyle">{{ item.price }} 만원</p>
+    <button @click="increaseAccusedCnt(item)">신고하기</button>
+    <span>신고수 : {{ item.accusedCnt }}</span>
   </div>
 </template>
 
@@ -17,17 +24,25 @@ export default {
       rooms : [
         {
           roomName : '갯마을',
-          price : 50
+          price : 50,
+          accusedCnt : 0
         },
         {
           roomName : '차차차',
-          price : 70
+          price : 70,
+          accusedCnt : 0
         },
         {
           roomName : '오징어',
-          price : 30
+          price : 30,
+          accusedCnt : 0
         },
       ]
+    }
+  },
+  methods:{
+    increaseAccusedCnt(item){
+      item.accusedCnt++;
     }
   },
   components: {
@@ -43,5 +58,16 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.menu{
+  background: darkslateblue;
+  padding: 15px;
+  border-radius: 5px;
+}
+
+.menu a{
+  color: white;
+  padding: 10px;
 }
 </style>
