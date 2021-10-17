e77badd5e9adaac77be6d0ef75cd42da8482b0ba
ParkMyoungKyu <audrb0999@gmail.com>
2021-10-17 16:06:17 +0900

    데이터 바인딩

--- Alvin/project01/src/App.vue
+++ Alvin/project01/src/App.vue
@@ -1,17 +1,35 @@
 <template>
+  <img alt="Vue logo" src="./assets/logo.png">
   <div>
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+      <h4 class="red" :style="style.style01">{{ name.name01 }} 원룸</h4>
+      <p>{{ price.price01 }} 만원</p>
+  </div>
+  <div>
+      <h4 class="blue" style="color : blue">{{ name.name02 }} 원룸</h4>
+      <p>{{ price.price02 }} 만원</p>
+  </div>
+   -------------------- 숙제 --------------------
+  <div>
+    <h4 :style="style.style01">{{ products[0] }}</h4> - {{ price.price01 }} 만원
+    <h4 :style="style.style02">{{ products[1] }}</h4> - {{ price.price02 }} 만원
+    <h4 :style="style.style03">{{ products[2] }}</h4> - {{ price.price03 }} 만원
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+ 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data(){
+    return{
+      name : {name01 : 'Vue', name02 : 'JS'},
+      price : {price01 : 100, price02 : 150, price03 : 200},
+      style : {style01 : 'color : red', style02 : 'color : green', style03 : 'color : #00b8ff'},
+      products :['역삼동원룸','천호동원룸','마포구원룸']
+    }
+  },
+  components:{
+    
   }
 }
 </script>
