683ebcce2e1fb760e86e037c8d00070dc7a1e051
ParkMyoungKyu <audrb0999@gmail.com>
2021-10-24 14:54:27 +0900

    props,custom event

--- Alvin/project01/src/ImExport.vue
+++ Alvin/project01/src/ImExport.vue
@@ -1,16 +1,22 @@
 <template>
 
-  <div class="black-bg" v-if="modalchang == true">
-    <div class="white-bg">
-      <img :src="oneroominfo[pushNum].image" class="room-img"/>
-      <h4>{{ oneroominfo[pushNum].title }}</h4>
-      <p>{{ oneroominfo[pushNum].content }}</p>
-      <p>{{ oneroominfo[pushNum].price }} 원</p>
-      <button v-on:click="modalchang = false">닫기</button>
-    </div>
-  </div>
+<!--  <div className="black-bg" v-if="modalchang == true">-->
+<!--    <div className="white-bg">-->
+<!--      <img :src="oneroominfo[pushNum].image" class="room-img"/>-->
+<!--      <h4>{{ oneroominfo[pushNum].title }}</h4>-->
+<!--      <p>{{ oneroominfo[pushNum].content }}</p>-->
+<!--      <p>{{ oneroominfo[pushNum].price }} 원</p>-->
+<!--      <button v-on:click="modalchang = false">닫기</button>-->
+<!--    </div>-->
+<!--  </div>-->
 
-  <div class="menu">
+    <Modal :oneroominfo="oneroominfo"
+           :modalchang="modalchang"
+           :pushNum = "pushNum"
+           @closeModal="modalchang = false"
+    />
+
+  <div className="menu">
     <a v-for="menulist in menus" :key="menulist">{{ menulist }}</a><br>
   </div>
 
@@ -22,17 +28,24 @@
     <p> {{ oneroominfo[0].price }} 원</p>
   </div> -->
 
-<!--  <div v-for="(oneroominfo,i) in roominfo" :key="oneroominfo">-->
-<!--    <img :src="roominfo[i].image" class="room-img">-->
-<!--    <h4>{{ roominfo[i].title }}</h4>-->
-<!--    <p> {{ roominfo[i].price }} 원</p>-->
-<!--  </div>-->
+  <!--  <div v-for="(oneroominfo,i) in roominfo" :key="oneroominfo">-->
+  <!--    <img :src="roominfo[i].image" class="room-img">-->
+  <!--    <h4>{{ roominfo[i].title }}</h4>-->
+  <!--    <p> {{ roominfo[i].price }} 원</p>-->
+  <!--  </div>-->
 
-  <div v-for="(roominfo,i) in oneroominfo" :key="i">
-    <img :src="roominfo.image" class="room-img">
-    <h4 v-on:click ="modalchang = true; pushNum = i" >{{ roominfo.title }}</h4>
-    <p> {{ roominfo.price }} 원</p>
-  </div>
+  <Card @openModal="modalchang = true; pushNum = $event " :oneroominfo="oneroominfo[i]" v-for="(roominfo,i) in oneroominfo" :key="roominfo"/>
+  <!--  <Card :oneroominfo="oneroominfo[1]"/>-->
+  <!--  <Card :oneroominfo="oneroominfo[2]"/>-->
+  <!--  <Card :oneroominfo="oneroominfo[3]"/>-->
+  <!--  <Card :oneroominfo="oneroominfo[4]"/>-->
+  <!--  <Card :oneroominfo="oneroominfo[5]"/>-->
+
+<!--    <div v-for="(roominfo,i) in oneroominfo" :key="i">-->
+<!--      <img :src="roominfo.image" class="room-img">-->
+<!--      <h4 v-on:click ="modalchang = true; pushNum = i" >{{ roominfo.title }}</h4>-->
+<!--      <p> {{ roominfo.price }} 원</p>-->
+<!--    </div>-->
 
 
 </template>
@@ -41,51 +54,55 @@
 
 import data from './assets/oneroom.js';
 import discount from './components/Discount.vue';
+import modal from './components/Modal.vue';
+import card from './components/Card.vue';
 
 export default {
   name: 'App',
-  data(){
-    return{
-      products :['역삼동원룸','천호동원룸','마포구원룸'],
+  data() {
+    return {
+      products: ['역삼동원룸', '천호동원룸', '마포구원룸'],
 
-      menus : ['Home', 'Shop', 'About'],
+      menus: ['Home', 'Shop', 'About'],
 
-      singoNum : [0,0,0],
-      modalchang : false,
-      oneroominfo : data,
+      singoNum: [0, 0, 0],
+      modalchang: false,
+      oneroominfo: data,
 
-      pushData : 0,
+      pushNum: 0,
     }
   },
-  methods:{
-    increase(){
-      this.singoNum[0] ++;
+  methods: {
+    increase() {
+      this.singoNum[0]++;
     },
 
   },
-  components:{
-    Discount : discount,
+  components: {
+    Discount: discount,
+    Modal : modal,
+    Card: card,
   }
 }
 </script>
 
 <style>
 body {
-  margin : 0
+  margin: 0
 }
 
 div {
-  box-sizing : border-box;
+  box-sizing: border-box;
 }
 
-.discount{
+.discount {
   background: #eee;
   padding: 10px;
   margin: 10px;
   border-radius: 5px;
 }
 
-.black-bg{
+.black-bg {
   width: 100%;
   height: 100%;
   background: rgba(0, 0, 0, 0.5);
@@ -99,10 +116,12 @@ div {
   border-radius: 8px;
   padding: 20px;
 }
-.room-img{
+
+.room-img {
   width: 50%;
   margin-top: 40px;
 }
+
 #ImExport {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -118,7 +137,7 @@ div {
 }
 
 .menu a {
-  color :white;
+  color: white;
   padding: 10px;
 }
 
