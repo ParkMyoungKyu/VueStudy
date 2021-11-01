7db65d032fab7b6131196640748a33e058ad1958
kwondahoon <dhk950211@gmail.com>
2021-11-01 23:34:25 +0900

    watcher로 데이터 감시

--- Owen/vuedongsan/src/Modal.vue
+++ Owen/vuedongsan/src/Modal.vue
@@ -1,18 +1,19 @@
 <template>
   <div class="black-bg" v-if="모달창열렸니==true">
-      <div class="white-bg">
+      <div class="white-bg" >
         <img :src="onerooms[inputTitle].image" style="width:100%" >
         <h4>{{ onerooms[inputTitle].title }}</h4>
         <p>{{ onerooms[inputTitle].content }}</p>
         <!-- <input @input="month = $event.target.value"> -->
-        <input v-model.number="month">
-        <input :value="keyword" @input="changeKeyword"/>
+        <input :value="month" @input="changeKeyword"/>
+        <!-- <input :value="keyword" @input="changeKeyword"/> -->
         <!-- <textarea v-model="month"></textarea> -->
         <!-- <select v-model="month">
           <option></option>
         </select> -->
+        <input type="range" min="1" max="12">
         <p> {{ month }}개월 선택함 :  {{ onerooms[inputTitle].price * month }}만원</p>
-        <p>keyword:{{ keyword }}</p>
+        <!-- <p>keyword:{{ keyword }}</p> -->
         <button @click="send">닫기</button>
       </div>
     </div>
@@ -27,6 +28,17 @@ export default {
         keyword:'',
       }
     },
+    watch :{
+      month(a){
+        // a 변경 후
+        // b 변경 전
+        let w = a;
+        alert(w);
+        if (a >= 13){
+          alert("입력한 수가 너무 큽니다.");
+        }
+      },
+    },
     props :{
       onerooms : Array,
       inputTitle : Number,
@@ -37,7 +49,7 @@ export default {
         this.$emit('closeModal')
       },
       changeKeyword(e){
-        this.keyword = e.target.value
+        e.target.value = e.target.value.replace(/[^0-9.]/g, '1').replace(/(\..*)\./g, '1');
       }
     },
 }
