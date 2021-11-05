e19d6bad80c9441e8df0612cfa7ca94cbcfa59c1
ParkMyoungKyu <audrb0999@gmail.com>
2021-11-05 21:16:16 +0900

    02 Bootstrap 적용 및 props 복습

---
+++ Alvin/blog/src/components/List.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h5>{{ blogInfo.title }}</h5>
+    <p>{{ blogInfo.date }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "List",
+  data(){
+    return {
+
+    }
+  },
+  props : {
+    blogInfo : Array,
+  }
+
+}
+</script>
+
+<style>
+
+</style>
