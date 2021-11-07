db5af8d078a4f8d36e298165dc59e13c05ef7260
kwondahoon <dhk950211@gmail.com>
2021-11-08 00:18:13 +0900

    start part3

---
+++ Owen/blogs/src/components/List.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+      <h5>{{blogList[0].title}}</h5>
+      <p>{{blogList[0].date}}</p>
+  </div>
+  <div>
+      <h5>{{blogList[1].title}}</h5>
+      <p>{{blogList[1].date}}</p>
+  </div>
+  <div>
+      <h5>{{blogList[2].title}}</h5>
+      <p>{{blogList[2].date}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+    name : 'list',
+    props : {
+        blogList : Array
+    }
+}
+</script>
+
+<style>
+
+</style>
