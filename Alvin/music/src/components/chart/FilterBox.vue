474660f428aa77286b92543b1da1e63707a60ef1
ParkMyoungKyu <audrb0999@naver.com>
2021-11-17 23:11:48 +0900

    인스타그램 필터기능 만들기

---
+++ Alvin/music/src/components/chart/FilterBox.vue
@@ -0,0 +1,31 @@
+<template>
+  <div style="display: inline-flex" :class="filter + ' filter-01'" v-for="filter in filters" :key="filter" :style="`background-image: url(${img})`">
+    {{ filter }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: "FilterBox",
+  data(){
+    return{
+
+    }
+  },
+  props:{
+    img : Array,
+    filters: Array,
+  }
+
+}
+</script>
+
+<style>
+  .filter-01{
+    width: 200px;
+    height: 200px;
+    margin: 50px 5px;
+    background-repeat: no-repeat; /*반복 제어*/
+    background-size: contain; /*너비랑 높이가 내용 안쪽에 알맞게 이미지 크기 적용*/
+  }
+</style>
--- Alvin/music/src/components/chart/MyList.vue
+++ Alvin/music/src/components/chart/MyList.vue
@@ -2,7 +2,7 @@
   <div class="p-3 mb-2 text-black text-center">
     <div class="btn btn-outline-dark ms-4">
       <input @change="upload" type="file" id="file1" style="display:none;">
-      <label for="file1" class="nav-fill">파일올리기</label>
+      <label for="file1" class="nav-fill">파일올리기<br>Filter</label>
     </div>
     <div class="btn btn-outline-dark ms-4">
       <input @change="upload" type="file" multiple id="file2" style="display:none;">
@@ -17,15 +17,21 @@
       <label for="file4" class="nav-fill">axios 이미지 올리기</label>
     </div>
     <div v-for="img_list in img" :key="img_list">
-      <div class="img-area" v-bind:style="`background-image:url(${img_list})`"></div>
+      <div class="img" v-bind:style="`background-image:url(${img_list})`"></div>
     </div>
+
+    <div v-if="show">
+      <FilterBox v-bind:img="img" v-bind:filters="filters"/>
+    </div>
+
     <div v-for="img_list in axiosImg" :key="img_list">
-      <div class="img-area" v-bind:style="`background-image:url(${img_list.imgList[2].url})`"></div>
+      <div class="img-area rounded-pill" v-bind:style="`background-image:url(${img_list.imgList[2].url})`"></div>
     </div>
   </div>
 </template>
 
 <script>
+import FilterBox from '../chart/FilterBox.vue';
 import axios from "axios";
 
 export default {
@@ -34,17 +40,23 @@ export default {
     return{
       img : [],
       axiosImg : "",
+      show : false,
       style : { b_img:'background-img:none', width: 'width:300px', height: 'height:300px', margin:'margin: 50px auto'},
+      filters : [ "aden", "_1977", "brannan", "brooklyn", "clarendon", "earlybird", "gingham", "hudson",
+        "inkwell", "kelvin", "lark", "lofi", "maven", "mayfair", "moon", "nashville", "perpetua",
+        "reyes", "rise", "slumber", "stinson", "toaster", "valencia", "walden", "willow", "xpro2"]
     }
   },
   methods:{
     upload(e){
       let file = e.target.files;
       //FileReader() // 1) 파일을 글자로 변환해줌
+      this.img = []; // 초기화
       for(let i=0; i < file.length; i++){
-        let url = URL.createObjectURL(file[i]) // 2) 이미지의 가상 URL을 생성해줌
+        const url = URL.createObjectURL(file[i]) // 2) 이미지의 가상 URL을 생성해줌
         console.log(url);
         this.img.push(url);
+        this.show = true;
       }
     },
     axiosUpload(){
@@ -57,17 +69,27 @@ export default {
       .catch(err =>{
           console.log(err);
       })
-    }
-
+    },
+  },
+  components:{
+    FilterBox,
   }
 }
 </script>
 
 <style>
-.img-area{
+.img{
   width: 300px;
   height: 300px;
-  margin: 50px auto;
+  margin: 20px auto;
+  background-repeat: no-repeat; /*반복 제어*/
+  background-size: contain; /*너비랑 높이가 내용 안쪽에 알맞게 이미지 크기 적용*/
+}
+.img-area{
+  width: 150px;
+  height: 150px;
+  margin: 20px;
+  float: left;
   background-repeat: no-repeat; /*반복 제어*/
   background-size: contain; /*너비랑 높이가 내용 안쪽에 알맞게 이미지 크기 적용*/
 }
