1b2155bf4f6adc887916012e94b99c5335bf4aaf
kwondahoon <dhk950211@gmail.com>
2021-11-28 20:06:35 +0900

    study 3

---
+++ Owen/vuestagram/src/components/Container.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+  <Post :instarData="instarData[0]"/>
+  <!-- <Post/>
+  <Post/> -->
+  {{instarData}}
+</div>
+</template>
+
+<script>
+import Post from './Post.vue'
+
+export default {
+    name: "Container",
+  components: { 
+    Post : Post,
+    
+  },
+  props :{
+      instarData : Object,
+  }
+}
+</script>
+
+<style>
+
+</style>
